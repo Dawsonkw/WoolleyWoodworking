b9ec7f1a8cafb0f8f618b5285170c22d67e15f7d
Dawsonkw <dawsonkw@gmail.com>
2022-12-05 12:19:48 -0700

    initial commit for  Woolley Woodworking

---
+++ woolley-woodworking/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      
+      <h1 className='text-teal-500'>Woolley Woodworking</h1>
+      <h2>Lorem Ipsum Doler Sit Amet rattatata</h2>
+      {/* Use these as an example template for website design https://www.tallguywoodworking.com/#  and   https://customfurnituredfw.com/ */}
+    </div>
+  )
+}
+
+export default App
