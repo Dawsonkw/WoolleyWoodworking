cf201b05e2155048b1d34e50093593502d8247e3
Dawsonkw <dawsonkw@gmail.com>
2022-12-12 14:49:27 -0700

    Continued building webpage, Instituted react PhotoGallery in a new component

--- woolley-woodworking/src/App.jsx
+++ woolley-woodworking/src/App.jsx
@@ -1,24 +1,27 @@
 import { useState } from 'react'
 import './App.css'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import Navbar from './components/navbar.jsx'
+import MainSection from './components/MainSection';
+import PhotoGallery from './components/PhotoGallery';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <main className='bg-white px-10 md:px-20'>
+
+      <main className='bg-white'>
         <section className='min-h-screen'>
-          <nav className='bg-black py-9 mb-12 flex justify-between '>
-            <h1 className='text-3xl text-teal-500'>Woolley Woodworking</h1>
-            {/* You can later add in a link to the h1 element that returns to the landing */}
-            <ul className='flex items-center space-x-4'>
-              <li><a class='text-white' href="">Portfolio</a></li>
-              <li><a class='text-white' href="">About</a></li>
-              <li className='m'><a class='text-white' href="">Contact</a></li>
-            </ul>
-          </nav>
+          {/* First component will be a navbar that will include the title 'Woolley Woodworking', and links for Profile, About and Contact*/}
+          <div>
+            <Navbar />         
+          </div>
         </section>
-        
+        <MainSection />
+        <PhotoGallery />
         <h2>Lorem Ipsum Doler Sit Amet rattatata pokedex jiu jistu Bulbasaur Dinodon Charmander Argonauts</h2>
       </main>
 
@@ -28,4 +31,8 @@ function App() {
 
 export default App
 
-{/* Use these as an example template for website design https://www.tallguywoodworking.com/#  and   https://customfurnituredfw.com/ */}
+//  Use these as an example template for website design https://www.tallguywoodworking.com/#  and   https://customfurnituredfw.com/ 
+
+// We're going to want to turn the pictures into a list of objects w/ URLsrc, name of piece, description, and alt 
+// then we're going to need to loop through the list and render each image component with it's info (See the example in the react docs)
+// photo gallery 
