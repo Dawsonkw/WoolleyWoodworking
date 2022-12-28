5fe84ceb57cccacb812901c2dfc0df396ce3f22b
Dawsonkw <dawsonkw@gmail.com>
2022-12-27 18:06:05 -0700

    updated photo gallery to get rid of the react responsive masonry. created a new component that displays the photos in a gallery style format

--- woolley-woodworking/src/App.jsx
+++ woolley-woodworking/src/App.jsx
@@ -8,8 +8,56 @@ import Navbar from './components/navbar.jsx'
 import MainSection from './components/MainSection';
 import PhotoGallery from './components/PhotoGallery';
 import Contact from './components/Contact';
+import Sender from './components/Sender';
 
 function App() {
+  const images = [
+    "/src/assets/IMG-3820.jpg",
+    "/src/assets/IMG-3968.jpg",
+    "/src/assets/IMG-3969.jpg",
+    "/src/assets/IMG-3970.jpg",
+    "/src/assets/IMG-3971.jpg",
+    "/src/assets/IMG-3972.jpg",
+    "/src/assets/IMG-3973.jpg",
+    "/src/assets/IMG-3980.jpg",
+    "/src/assets/IMG-5576.jpg",
+    "/src/assets/IMG-5577.jpg",
+    "/src/assets/IMG-5579.jpg",
+    "/src/assets/IMG-5580.jpg",
+    "/src/assets/IMG-5582.jpg",
+    "/src/assets/IMG-5583.jpg",
+    "/src/assets/IMG-5584.jpg",
+    "/src/assets/IMG-5585.jpg",
+    "/src/assets/IMG-5763.jpg",
+    "/src/assets/IMG-5764.jpg",
+    "/src/assets/IMG-5765.jpg",
+    "/src/assets/IMG-5766.jpg",
+    "/src/assets/IMG-5767.jpg",
+    "/src/assets/IMG-5768.jpg",
+    "/src/assets/IMG-5869.jpg",
+    "/src/assets/IMG-5870.jpg",
+    "/src/assets/IMG-5871.jpg",
+    "/src/assets/IMG-5872.jpg",
+    "/src/assets/IMG-5873.jpg",
+    "/src/assets/IMG-5874.jpg",
+    "/src/assets/IMG-5875.jpg",
+    "/src/assets/IMG-5876.jpg",
+    "/src/assets/IMG-5877.jpg",
+    "/src/assets/IMG-5878.jpg",
+    "/src/assets/IMG-5879.jpg",
+    "/src/assets/IMG-5880.jpg",
+    "/src/assets/IMG-5881.jpg",
+    "/src/assets/IMG-5882.jpg",
+    "/src/assets/IMG-5883.jpg",
+    "/src/assets/IMG-5884.jpg",
+    "/src/assets/IMG-5885.jpg",
+    "/src/assets/IMG-5891.jpg",
+    "/src/assets/IMG-5892.jpg",
+    "/src/assets/IMG-5893.jpg",
+    "/src/assets/IMG-5894.jpg",
+    "/src/assets/IMG-5895.jpg",
+    "/src/assets/IMG-5898.jpg",
+]
 
   return (
     <div className="App">
@@ -19,8 +67,9 @@ function App() {
           <div>
             <Navbar />         
           </div>
-          <PhotoGallery />
+          <PhotoGallery images={images} />
           <Contact />
+          {/* <Sender /> */}
         </section>
         <MainSection />
         
--- woolley-woodworking/src/components/Contact.jsx
+++ woolley-woodworking/src/components/Contact.jsx
@@ -3,6 +3,34 @@ import {FiPhoneCall} from 'react-icons/fi'
 import {HiOutlineMail} from 'react-icons/hi'
 import {VscLocation} from 'react-icons/vsc'
 import { FiFacebook } from "react-icons/fi";
+import emailjs from 'emailjs-com';
+import Swal from 'sweetalert2';
+
+const SERVICE_ID = "service_r5ieg6y";
+const TEMPLATE_ID = "template_lufahf9";
+const USER_ID = "0DEv7bd_vZqmZbu0H";
+
+
+const handleOnSubmit = (e) => {
+    e.preventDefault();
+    emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, e.target, USER_ID)
+      .then((result) => {
+        console.log(result.text);
+        Swal.fire({
+          icon: 'success',
+          title: 'Message Sent Successfully'
+        })
+      }, (error) => {
+        console.log(error.text);
+        Swal.fire({
+          icon: 'error',
+          title: 'Ooops, something went wrong',
+          text: error.text,
+        })
+      });
+    e.target.reset()
+  };
+
 
 class Contact extends Component {
     render() {
@@ -44,9 +72,9 @@ class Contact extends Component {
                             <div className='absolute w-40 h-40 bg-tan rounded-full -right-20 -top-20'></div>
                             <div className='absolute w-40 h-40 bg-tan rounded-full -left-20 -bottom-20'></div>
                             <div className='relative z-10 bg-white rounded-xl shadow-lg p-8 text-gray-600 md:w-80 '>
-                                <form action="" className='flex flex-col space-y-4'>
+                                <form onSubmit={handleOnSubmit}  action="" className='flex flex-col space-y-4'>
                                     <div>
-                                        <label htmlFor="" className='text-sm'>Your Name</label>
+                                        <label name='to_name' htmlFor="" className='text-sm'>Your Name</label>
                                     </div>
                                         <div>
                                             <input type="text" placeholder='Your Name' className='ring-1 ring-gray-300 w-full rounded-md px-4 py-2 outline-none focus:ring-2 focus:ring-gray-500' />
@@ -67,7 +95,7 @@ class Contact extends Component {
                                         <textarea type="text" placeholder='Type a brief message here' rows={4} className='ring-1 ring-gray-300 w-full rounded-md px-4 py-2 outline-none focus:ring-2 focus:ring-gray-500'></textarea>
                                     </div>
                                     </div>
-                                    <button className='inline-block self-end bg-tan text-white font-bold rounded-lg px-6 py-2 uppercase text-sm'>Send Message</button>
+                                    <button type='submit' className='inline-block self-end bg-tan text-white font-bold rounded-lg px-6 py-2 uppercase text-sm'>Send Message</button>
                                 </form>
                             </div>
                         </div>
