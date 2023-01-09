7a85a203397916e91558d8edceeacfabe8c15438
Dawsonkw <dawsonkw@gmail.com>
2023-01-09 09:34:38 -0700

    broke it then fixed it.

--- woolley-woodworking/src/App.jsx
+++ woolley-woodworking/src/App.jsx
@@ -10,53 +10,6 @@ import PhotoGallery from './components/PhotoGallery';
 import Contact from './components/Contact';
 
 function App() {
-  const images = [
-    "/src/assets/IMG-3820.jpg",
-    "/src/assets/IMG-3968.jpg",
-    "/src/assets/IMG-3969.jpg",
-    "/src/assets/IMG-3970.jpg",
-    "/src/assets/IMG-3971.jpg",
-    "/src/assets/IMG-3972.jpg",
-    "/src/assets/IMG-3973.jpg",
-    "/src/assets/IMG-3980.jpg",
-    "/src/assets/IMG-5576.jpg",
-    "/src/assets/IMG-5577.jpg",
-    "/src/assets/IMG-5579.jpg",
-    "/src/assets/IMG-5580.jpg",
-    "/src/assets/IMG-5582.jpg",
-    "/src/assets/IMG-5583.jpg",
-    "/src/assets/IMG-5584.jpg",
-    "/src/assets/IMG-5585.jpg",
-    "/src/assets/IMG-5763.jpg",
-    "/src/assets/IMG-5764.jpg",
-    "/src/assets/IMG-5765.jpg",
-    "/src/assets/IMG-5766.jpg",
-    "/src/assets/IMG-5767.jpg",
-    "/src/assets/IMG-5768.jpg",
-    "/src/assets/IMG-5869.jpg",
-    "/src/assets/IMG-5870.jpg",
-    "/src/assets/IMG-5871.jpg",
-    "/src/assets/IMG-5872.jpg",
-    "/src/assets/IMG-5873.jpg",
-    "/src/assets/IMG-5874.jpg",
-    "/src/assets/IMG-5875.jpg",
-    "/src/assets/IMG-5876.jpg",
-    "/src/assets/IMG-5877.jpg",
-    "/src/assets/IMG-5878.jpg",
-    "/src/assets/IMG-5879.jpg",
-    "/src/assets/IMG-5880.jpg",
-    "/src/assets/IMG-5881.jpg",
-    "/src/assets/IMG-5882.jpg",
-    "/src/assets/IMG-5883.jpg",
-    "/src/assets/IMG-5884.jpg",
-    "/src/assets/IMG-5885.jpg",
-    "/src/assets/IMG-5891.jpg",
-    "/src/assets/IMG-5892.jpg",
-    "/src/assets/IMG-5893.jpg",
-    "/src/assets/IMG-5894.jpg",
-    "/src/assets/IMG-5895.jpg",
-    "/src/assets/IMG-5898.jpg",
-]
 
   return (
     <div className="App">
@@ -66,9 +19,8 @@ function App() {
           <div>
             <Navbar />         
           </div>
-          <PhotoGallery images={images} />
+          <PhotoGallery />
           <Contact />
-          
         </section>
         <MainSection />
         
--- woolley-woodworking/tailwind.config.cjs
+++ woolley-woodworking/tailwind.config.cjs
@@ -16,15 +16,7 @@ module.exports = {
         '1/2': '300px',
         '3/4': '75%',
         '90/1': '90%',
-      },
-      colors: {
-        'regal-blue': '#243c5a',
-        'tan': '#b68d40',
-        'cream': '#f4ebd0',
-        'charcoal': '#122620',
-        'gold': '#d6ad60',
-        'forestgreen': '#104210',
-      },
+      }
     },
   },
   plugins: [],
